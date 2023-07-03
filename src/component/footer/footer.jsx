dd792383c4bd2c29a986bec06d7e0c2a5eccccf7
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 19:48:00 +0330

    import ItemFooter component

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,12 @@
+
+import ItemFooter from "../itemFooter/itemFooter";
+
+const Footer =()=>{
+    return(
+        <div className="bg-[#0F172B]">
+            <ItemFooter/>
+        </div>
+    )
+}
+
+export default Footer;
