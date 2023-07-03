0d0e3b75e8707d5cd7a60a76b7da93d6f9971803
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 20:22:35 +0330

    add Item footer component

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,10 +1,14 @@
 
 import ItemFooter from "../itemFooter/itemFooter";
+import { company,contect, opening, newLetter  } from "../../data/data";
 
 const Footer =()=>{
     return(
-        <div className="bg-[#0F172B]">
-            <ItemFooter/>
+        <div className="bg-[#0F172B] flex justify-between">
+            <ItemFooter title={'Companey'} data={company}/>
+            <ItemFooter title={'Contact'} data={contect}/>
+            <ItemFooter title={'Opening'} data={opening}/>
+            <ItemFooter title={'NewsLetter'} data={newLetter}/>
         </div>
     )
 }
