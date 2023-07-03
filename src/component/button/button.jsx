5d663a05ecbc998f453827c3f2e18163eb5313a5
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 19:44:32 +0330

    create and style

---
+++ src/component/button/button.jsx
@@ -0,0 +1,10 @@
+
+const Button =({title})=>{
+    return(
+        <button className="w-[160px] h-[40px] rounded-[2px] font-Nunito bg-[#FEA116] text-white px-[24] py-[8]" >
+            {title}
+        </button>
+    )
+}
+
+export default Button;
