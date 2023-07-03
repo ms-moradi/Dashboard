9c1b7ab2a9d61e6eabd4c5dd142d179053f35a27
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 20:22:53 +0330

    delete data

--- src/component/itemFooter/itemFooter.jsx
+++ src/component/itemFooter/itemFooter.jsx
@@ -1,31 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const itemsfooter1 =[
-    {id:1, title:'About Us'},
-    {id:2, title:'Contact Us'},
-    {id:3, title:'Reservation'},
-    {id:4, title:'Privacy Policy'},
-    {id:5, title:'Terms & Condition'},
-]
 
-const ItemFooter =({})=>{
+
+
+const ItemFooter =({title, data})=>{
     return(
         <div>
             <div className="flex justify-start items-start mb-6">
-            <h4 className="text-[#FEA116] text-[24px] font-Pacifico ">Company</h4>
+            <h4 className="text-[#FEA116] text-[24px] font-Pacifico ">{title}</h4>
             <p className="text-[#FEA116]"> _____</p>
             </div>
             <div>
                 <ul>
-                    {itemsfooter1.map((item)=>(
+                    {data.map((item)=>(
                         <li key={item.id} className="text-white flex justify-start items-center gap-2 mb-1">
-                            <FontAwesomeIcon icon={faAngleRight} size='xs'/>
+                            <FontAwesomeIcon icon={item.icon} size='xs'/>
                             <p className="text-[15px] font-Nunito font-normal">{item.title}</p>
                         </li>
                     ))}
                 </ul>
-
             </div>
         </div>
         
