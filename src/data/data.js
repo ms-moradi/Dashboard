9f993cd608a7217634faf35c3fd60c9dd04f4972
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 20:21:57 +0330

    add data

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+
+export const company = [
+    {id:1, title:'About Us', icon:faAngleRight},
+    {id:2, title:'Contact Us', icon:faAngleRight},
+    {id:3, title:'Reservation', icon:faAngleRight},
+    {id:4, title:'Privacy Policy', icon:faAngleRight},
+    {id:5, title:'Terms & Condition', icon:faAngleRight},
+]
+
+export const contect = [
+    {id:1, title:'123 Street, New York, USA', icon:''},
+    {id:2, title:'+012 345 67890', icon:''},
+    {id:3, title:'info@example.com', icon:''},
+]
+
+export const opening = [
+    {id:1, title:'Monday - Saturday'},
+    {id:2, title:'09Am - 09PM'},
+    {id:3, title:'Sunday'},
+    {id:4, title:'10AM - 08PM'},
+]
+
+export const newLetter = [
+    {id:1, title:'Dolor ametsit justo elitr'},
+    {id:2, title:'clita ipsum elitr est.'},
+]
