79685d37d82d78609e3a8a25cc6a41cd951e055b
ms.moradi <ms.moradi28@gmail.com>
2023-07-03 19:42:59 +0330

    add icons

---
+++ src/assets/font/fontAwesome.js
@@ -0,0 +1,12 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faUtensils, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(
+  faUtensils,
+  faAngleRight
+
+);
